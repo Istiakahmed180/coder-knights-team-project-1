c84be68e399df5b8dd9b9a98ffb00eb18cc14f2e
shihab00027 <shihabhasan981@gmail.com>
2023-03-31 17:52:10 +0600

    Top selling product complete

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React, { useContext } from 'react';
 import { myContext } from '../../contextApi/Authcontext';
+import TopSellingProduct from './TopSellingProduct/TopSellingProduct';
 
 const Home = () => {
-    const {test} = useContext(myContext)
+    const { test } = useContext(myContext)
     return (
         <div>
             <h2>Home Page {test} </h2>
+            <TopSellingProduct></TopSellingProduct>
         </div>
     );
 };
--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Authcontext from './contextApi/Authcontext';
+import 'remixicon/fonts/remixicon.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
     <Authcontext> <App /> </Authcontext>
-  
+
 );
 
 reportWebVitals();
